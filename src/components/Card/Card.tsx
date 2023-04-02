ca01a2573b3ea9061a11f7fdade6071ebd8bda4c
TAN Lucie <36072438+LucieTan@users.noreply.github.com>
2023-04-03 00:20:47 +0200

    LouMoa Interface - Add Card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,25 @@
+
+
+interface CardProps {
+    width: number | string;
+    height: number | string;
+    color : string;
+    textColor?: string;
+    onClick?: () => void;
+    children: JSX.Element;
+}
+
+const Card = ({width, height, color, textColor, onClick, children}: CardProps) => {
+
+    return (
+        <div className={'card'} style={{
+            width: (typeof width !== 'string' ? `${width}px` : width),
+            height: (typeof height !== 'string' ? `${height}px` : height),
+            backgroundColor: color, color: textColor
+        }}
+             onClick={onClick}
+        >
+            {children}
+        </div>
+    );
+}
