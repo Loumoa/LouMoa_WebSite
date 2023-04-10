86ed11941c724b2d08fd6a928e413e4714a91a26
Paul MARANDE <paul.marande@efrei.net>
2023-04-10 19:28:32 +0200

    LouMoa Interface - Init HousingManagementPage

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@ import './App.css';
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage"
 import HousingPage from "./pages/HousingPage";
-
+import HousingManagementPage from './pages/HousingManagementPage/HousingManagementPage';
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LoginPage/>}/>
-            <Route path="/housing/:id" element={<HousingPage/>}/>
-            <Route path="/housing" element={<HousingPage/>}/>
+          <Route path="/housing/:id" element={<HousingPage/>}/>
+          <Route path="/housing" element={<HousingPage/>}/>
+          <Route path="/housingManagementPage" element={<HousingManagementPage/>}/>
           <Route path="*" element={<Navigate to="/" replace/>}/>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/HousingManagementPage/HousingManagementPage.tsx
@@ -0,0 +1,16 @@
+import './HousingManagementPage.css'
+//Import React
+import React, {useState} from 'react'
+
+//Import Librairie
+import {useNavigate} from 'react-router-dom'
+import Style from "styled-components"
+
+const HousingManagementPage = () => {
+    const menu = [{value: '/home', label: 'Accueil'}, {value: "/housing", label: 'Locations'}, {value: "/housingManagement", label : 'Gestion des biens'}]
+
+    return (<div>WIP</div>    )
+
+}
+
+export default HousingManagementPage
